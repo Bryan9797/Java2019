a5e04eb33521ad4e296f3721db363b9e1c4d8f2f
Bryan9797 <35985929+Bryan9797@users.noreply.github.com>
2019-02-08 19:37:14 -0700

    Primera clase

---
+++ FirstApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstapp;
+
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+/**
+ *
+ * @author bryan
+ */
+public class FirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola mundo \n"); //Sintaxis para imprimir mensaje
+        System.out.print("Hola mundo \n");
+    }
+    
+}
